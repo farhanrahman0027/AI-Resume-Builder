a6aad7e9ae574b759909768400bb1a09197ad7b2
farhanur rahman <farhanrahman0027@gmail.com>
2025-10-11 23:45:20 +0530

    add the component in home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import Banner from '../Components/Home/Banner'
 const Home = () => {
   return (
     <div>
-      <h1>Home page</h1>
+      <Banner/>
     </div>
   );
 };
