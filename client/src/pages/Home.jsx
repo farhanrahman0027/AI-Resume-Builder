7bd771029c1cd973c3c27a2b20b441ff63192def
farhanur rahman <farhanrahman0027@gmail.com>
2025-10-21 23:55:56 +0530

    add the testimonials

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Banner from '../Components/Home/Banner'
 import HeroSection from "../Components/Home/HeroSection";
 import Features from "../Components/Home/Features";
-
+import Testimonial from "../Components/Home/Testimonial";
 const Home = () => {
   return (
     <div>
       <Banner/>
       <HeroSection/>
       <Features/>
+      <Testimonial/>
     </div>
   );
 };
