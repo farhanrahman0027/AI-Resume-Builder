37ed6198c1ae7522d4c9a9ff316ffa657268b5be
farhanur rahman <farhanrahman0027@gmail.com>
2025-10-21 20:52:29 +0530

    add the hero section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Banner from '../Components/Home/Banner'
+import HeroSection from "../Components/Home/HeroSection";
 const Home = () => {
   return (
     <div>
       <Banner/>
+      <HeroSection/>
     </div>
   );
 };
